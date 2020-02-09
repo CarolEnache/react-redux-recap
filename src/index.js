102ccafe2550181bcd259966a33c93e50e9b4ec2
CarolEnache <enache.carol@gmail.com>
2020-02-09 14:40:15 +0000

    Async Action Creators

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { selectSubreddit, fetchPosts } from './redux/actions';
+import rootReducer from './redux/reducers';
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware, //lets you dispatch() functions
+    loggerMiddleware // neat middleware that logs actions
+  )
+);
+
+store.dispatch(selectSubreddit('reactjs'));
+store.dispatch(fetchPosts('reactjs')).then(() => console.log(store.getState()));
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
